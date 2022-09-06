c2b2f2b26862d4e4ec47aae73af0377e83f9af3c
tomter82 <105425566+tomter82@users.noreply.github.com>
2022-09-06 12:46:02 +0200

    Update final.py

--- final.py
+++ final.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-install sklearn
+
 import pickle 
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
