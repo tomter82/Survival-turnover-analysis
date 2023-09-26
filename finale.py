cb33d68ca6888118dbac8b81d0620a4a937d7bed
tomter82 <105425566+tomter82@users.noreply.github.com>
2023-09-26 13:42:55 +0200

    Update finale.py

--- finale.py
+++ finale.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import log_loss, roc_auc_score, recall_score, precision_score, average_precision_score, f1_score, classification_report, accuracy_score, plot_roc_curve, plot_precision_recall_curve, plot_confusion_matrix
+from sklearn.metrics import log_loss, roc_auc_score, recall_score, precision_score, average_precision_score, f1_score, classification_report, accuracy_score,  plot_precision_recall_curve, plot_confusion_matrix
 import pickle 
 
 # load model
