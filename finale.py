5df50b58107592f4f77eee11bc2dd7cc0ce4960a
tomter82 <105425566+tomter82@users.noreply.github.com>
2024-03-20 20:46:41 +0100

    Update finale.py

--- finale.py
+++ finale.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
