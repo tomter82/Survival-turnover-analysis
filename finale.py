482b17f44231d8d60f377be7824ed3e285a43b08
tomter82 <105425566+tomter82@users.noreply.github.com>
2024-03-22 16:37:59 +0100

    Update finale.py

--- finale.py
+++ finale.py
@@ -30,7 +30,7 @@ def main():
         </div>
         """
     st.markdown(html_temp,unsafe_allow_html=True) #a simple html 
-    profession=st.selectbox("profession area ",options=['BusinessDevelopment','Marketing' ,'IT' ,'HR' ,'other' ,' Consult' ,'Commercial','Manager','Finance','Engineer','Teaching','Accounting','Law','PR' ])
+    profession=st.selectbox("profession area ",options=['BusinessDevelopment','Marketing' ,'IT' ,'HR' ,'other' ,'Consult' ,'Commercial','manage','Finance','Engineer','Teaching','Accounting','Law','PR' ])
     extraversion=st.number_input("Extraversion",0.0,10.0,5.,step =.1)
     independ=st.number_input("Independ",0.0,10.0,5.,step =.1)
     selfcontrol=st.number_input("Selfcontrol",0.0,10.0,5.,step =.1)
