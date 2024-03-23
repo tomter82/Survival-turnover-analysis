b18f8724b8b56a3c4249c19e6e8756ef8742d801
tomter82 <105425566+tomter82@users.noreply.github.com>
2024-03-23 22:56:24 +0100

    Update finale.py

--- finale.py
+++ finale.py
@@ -62,8 +62,8 @@ def main():
         'selfcontrol':[selfcontrol],
         'anxiety':[anxiety],
         'novator':[novator]
-        })
-st.dataframe(data_new)
+    })
+
     
     data_new["gender"] = data_new["gender"].astype("category")
     data_new["profession"] = data_new["profession"].astype("category")
