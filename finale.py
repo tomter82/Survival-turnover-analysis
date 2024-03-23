c48e0bd1b63b9a73c5e9da728bdefb53331f5218
tomter82 <105425566+tomter82@users.noreply.github.com>
2024-03-24 00:31:49 +0100

    Update finale.py

--- finale.py
+++ finale.py
@@ -88,7 +88,7 @@ def main():
 
         
         #prediction=loaded_model.predict_proba(data_new)[:, 1]
-        #result=loaded_model.predict_proba(data_new)[:, 1].round(3)*100   #result will be displayed if pressed
+        result=loaded_model.predict_proba(data_new)[:, 1].round(3)*100   #result will be displayed if pressed
     st.success("Based on your profile ,your probability to still working in the Company is {}%" .format(result))
     
 if __name__=='__main__':
