b660b03b7f6d2a6113d505adcfcfd08df064ca4e
tomter82 <105425566+tomter82@users.noreply.github.com>
2024-03-24 00:36:20 +0100

    Update finale.py

--- finale.py
+++ finale.py
@@ -82,12 +82,10 @@ def main():
     
     result=""
     if st.button("Predict"):
-        result=predict_probability(stag, gender, age, industry, profession, traffic,
-        coach, head_gender, greywage, way, extraversion, independ,
-        selfcontrol, anxiety, novator)
+        result=predict_probability(stag, gender, age, industry, profession, traffic,coach, head_gender, greywage, way, extraversion, independ, selfcontrol, anxiety, novator)
 
         
-        #prediction=loaded_model.predict_proba(data_new)[:, 1]
+        prediction=loaded_model.predict_proba(data_new)[:, 1]
         result=loaded_model.predict_proba(data_new)[:, 1].round(3)*100   #result will be displayed if pressed
     st.success("Based on your profile ,your probability to still working in the Company is {}%" .format(result))
     
