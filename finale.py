790478e827a0480347122129bd5417a7612dfc84
tomter82 <105425566+tomter82@users.noreply.github.com>
2024-03-23 13:23:40 +0100

    Update finale.py

--- finale.py
+++ finale.py
@@ -17,7 +17,7 @@ import pickle
 
 # load model
 import pickle
-loaded_model = pickle.load(open('C:/Users/thoma/Desktop/final project/turnover//clf_pipe8.sav', 'rb'))
+loaded_model = pickle.load(open('clf_pipe8.sav', 'rb'))
 
 def predict_probability(stag,gender,age,profession,traffic,coach,greywage,way,extraversion,independ,selfcontrol,anxiety,novator):
     prediction=loaded_model.predict_proba(data_new)[:, 1]#predictions using our model
