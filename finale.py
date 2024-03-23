d5b8b7d5231cb23548246cc478a32780f129b0c1
tomter82 <105425566+tomter82@users.noreply.github.com>
2024-03-24 00:48:41 +0100

    Update finale.py

--- finale.py
+++ finale.py
@@ -1,30 +1,25 @@
-import streamlit as st
-import pandas as pd
 import numpy as np
-import sklearn
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import train_test_split
-from sklearn.impute import SimpleImputer
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.pipeline import make_pipeline
+import pandas as pd
+import pickle 
+import streamlit as st
 
-from sklearn.linear_model import LogisticRegression
-from sklearn import datasets, metrics, model_selection, svm
-from sklearn.metrics import log_loss, roc_auc_score, recall_score, precision_score, average_precision_score, f1_score, classification_report, accuracy_score
-
-# load model
-import pickle
-#loaded_model = pickle.load(open('clf_pipe5.sav', 'rb'))
-loaded_model = pickle.load(open('logit.pkl', 'rb'))
-def predict_probability(stag,gender,age,industry,profession,traffic,coach,head_gender,greywage,way,extraversion,independ,selfcontrol,anxiety,novator):
-    prediction=loaded_model.predict_proba(data_new)[:, 1]#predictions using our model
+pickle_a=open("logit.pkl","rb")
+regressor=pickle.load(pickle_a) # our model
+#event var 
+def predict_chance(stag, gender, age, industry, profession, traffic,
+       coach, head_gender, greywage, way, extraversion, independ,
+       selfcontrol, anxiety, novator):
+    prediction=regressor.predict([[stag, event, gender, age, industry, profession, traffic,
+       coach, head_gender, greywage, way, extraversion, independ,
+       selfcontrol, anxiety, novator]]) #predictions using our model
     return prediction 
+
+
 def main():
-    st.title("We are hiring!") #simple title for the app
+    st.title("Probability APP using ML") #simple title for the app
     html_temp="""
         <div>
-        <h2></h2>
+        <h2> Prediction ML app</h2>
         </div>
         """
     st.markdown(html_temp,unsafe_allow_html=True) #a simple html 
@@ -43,47 +38,12 @@ def main():
     coach = st.selectbox("coach",options=['no' , 'yes' ,'my head'])
     industry=st.selectbox("industry",options=['Retail','manufacture' ,'IT' ,'Banks' ,'etc' ,'Consult' ,'State','Building','PowerGeneration','transport','Telecom','Mining','Pharma','Agriculture' ,'RealEstate',' HoReCa' ])
     profession=st.selectbox("profession",options=['BusinessDevelopment','Marketing' ,'IT' ,'HR' ,'etc' ,'Consult' ,'Commercial','manage','Finanñe','Engineer','Teaching','Accounting','Law','PR' ])
-    
-    
-    
-    import pandas as pd
-    data_new = pd.DataFrame({
-        'stag':[stag],
-        'gender':[gender],
-        'age':[age],
-        'industry':[industry],
-        'profession':[profession],
-        'traffic':[traffic],
-        'coach':[coach], 
-        'head_gender':[head_gender],
-        'greywage':[greywage],
-        'way':[way],
-        'extraversion':[extraversion], 
-        'independ':[independ], 
-        'selfcontrol':[selfcontrol],
-        'anxiety':[anxiety],
-        'novator':[novator]
-    })
-
-    
-    data_new["gender"] =  data_new["gender"].astype("category")
-    data_new["profession"] =  data_new["profession"].astype("category")
-    data_new["traffic"] =  data_new["traffic"].astype("category")
-    data_new["coach"] =  data_new["coach"].astype("category")
-    data_new["head_gender"] =  data_new["head_gender"].astype("category")
-    data_new["greywage"] =  data_new["greywage"].astype("category")
-    data_new["way"] =  data_new["way"].astype("category")
-    data_new["industry"] =  data_new["industry"].astype("category")
-    
     result=""
     if st.button("Predict"):
-       # result=predict_probability(stag, gender, age, industry, profession, traffic,coach, head_gender, greywage, way, extraversion, independ, selfcontrol, anxiety, novator)
-
+        result=predict_chance(stag, gender, age, industry, profession, traffic,
+        coach, head_gender, greywage, way, extraversion, independ,
+        selfcontrol, anxiety, novator) #result will be displayed if button is pressed
+    st.success("The probability to stay in the company  is{}".format(result))
         
-        #prediction=loaded_model.predict_proba(data_new)[:, 1]
-        result=loaded_model.predict_proba(data_new)[:, 1].round(3)*100   #result will be displayed if pressed
-    st.success("Based on your profile ,your probability to still working in the Company is {}%" .format(result))
-    
 if __name__=='__main__':
     main()
-   
