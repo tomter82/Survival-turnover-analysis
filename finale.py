aa345162f5778998016a9532cb62a3759282084f
tomter82 <105425566+tomter82@users.noreply.github.com>
2024-03-24 13:51:40 +0100

    Update finale.py

--- finale.py
+++ finale.py
@@ -19,7 +19,7 @@ import pickle
 import pickle
 loaded_model = pickle.load(open('clf_pip.sav', 'rb'))
 #loaded_model = pickle.load(open('logit1.pkl', 'rb'))
-def predict_probability(stag,gender ,age,profession,traffic,coach,greywage,way,extraversion,independ,selfcontrol,anxiety,novator):
+def predict_probability(stag,gender,age,profession,traffic,coach,greywage,way,extraversion,independ,selfcontrol,anxiety,novator):
     prediction=loaded_model.predict_proba(data_new)[:, 1]#predictions using our model
     return prediction 
 def main():
@@ -46,7 +46,6 @@ def main():
    
     import pandas as pd
     data_new = pd.DataFrame({
-        
         'stag':[stag],
         'gender':[gender],
         'age':[age],
