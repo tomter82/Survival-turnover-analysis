3e088136808a54b1a9dc2ad4d4004f644944629d
tomter82 <105425566+tomter82@users.noreply.github.com>
2024-03-24 21:07:37 +0100

    Update finale.py

--- finale.py
+++ finale.py
@@ -32,7 +32,7 @@ def main():
     st.markdown(html_temp,unsafe_allow_html=True) #a simple html 
     gender=st.selectbox("Sex",options=['f', 'm' ])
     age=st.slider("Age", 15, 67,step = 1)
-    stag=st.slider("Prevision time", 0, 180,step = 30,help = "Set the time to prediction from 1 month to 6 months")
+    profession=st.selectbox("profession area ",options=['BusinessDevelopment','Marketing' ,'IT' ,'HR' ,'other' ,'Consult' ,'Commercial','manage','Finance','Engineer','Teaching','Accounting','Law','PR'  ])
     extraversion=st.number_input("Extraversion",0.0,10.0,5.,step =.1)
     independ=st.number_input("Independ",0.0,10.0,5.,step =.1)
     selfcontrol=st.number_input("Selfcontrol",0.0,10.0,5.,step =.1)
@@ -41,9 +41,9 @@ def main():
     greywage = st.selectbox("Wage",options=['contract', 'self_emp' ])
     traffic = st.selectbox("Contact",options=['From friend' , 'Advertising' ,'Recruiting agency','Direct contact' , 'Friends in the company' , 'From company employee','Company contact','Job site'])
     coach = st.selectbox("Coach",options=['no' , 'yes' ,'my head'],help = "select if the cantidate will have a surpervisor/no supervisor /or will be a supervisor" )
-    profession=st.selectbox("profession area ",options=['BusinessDevelopment','Marketing' ,'IT' ,'HR' ,'other' ,'Consult' ,'Commercial','manage','Finance','Engineer','Teaching','Accounting','Law','PR' ])
-    way= st.selectbox("Way to work",options=['bus', 'car', 'foot'])
    
+    way= st.selectbox("Way to work",options=['bus', 'car', 'foot'])
+    stag=st.slider("Prevision time", 0, 180,step = 30,help = "Set the time to prediction from 1 month to 6 months")
     import pandas as pd
     data_new = pd.DataFrame({
         'stag':[stag],
